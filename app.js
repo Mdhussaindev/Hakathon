00eed509e37afd7f773262d566cb25d72765b0e2
Muhammad.Hussain.Khan <mdhussain.dev00@gmail.com>
2026-02-13 10:29:57 +0500

    update

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", function(){
+    navLinks.classList.toggle("active");
+  });
