43aaff26f616803c0ad7133da024d9b75ac3bf82
Muhammad.Hussain.Khan <mdhussain.dev00@gmail.com>
2026-02-13 11:27:18 +0500

    update

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-  if(menuToggle && navLinks){
-    menuToggle.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  }
+menuToggle.addEventListener("click", function() {
+  navLinks.classList.toggle("active");
+  menuToggle.classList.toggle("active");
 });
-
-const menuToggle = document.getElementById("menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  menuToggle.addEventListener("click", function(){
-    navLinks.classList.toggle("active");
-  });
